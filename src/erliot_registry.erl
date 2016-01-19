efd07a0c58ffe0ad2fbf8776ab3bc19d65840d14
Alexander Wingård <alexander.wingard@gmail.com>
2016-01-19 13:57:16 +0100

    Add erliot registry

---
+++ src/erliot_registry.erl
@@ -0,0 +1,50 @@
+-module(erliot_registry).
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {multisocket}).
+-record(udp, {socket, ip, inportno, packet}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+init([]) ->
+    {ok, S} = gen_udp:open(6000, [{active, true}, {mode, binary}]),
+    {ok, #state{multisocket = S}}.
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(#udp{packet = P}, State) ->
+    io:format("~p~n",[binary_to_term(P)]),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
