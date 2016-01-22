90df2d8e78fdf5e42d7a1b370502321f74e69516
Alexander Wingård <lex@nirun>
2016-01-22 15:53:43 +0100

    Rename ws handler

--- src/erliot_app.erl
+++ src/erliot_app.erl
@@ -9,7 +9,7 @@ start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
 		{'_', [
 			{"/", cowboy_static, {file, "priv/index.html"}},
-			{"/websocket", ws_handler, []}
+			{"/websocket", erliot_ws_handler, []}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
