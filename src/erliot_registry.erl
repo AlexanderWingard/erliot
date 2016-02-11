dbb1d0a4fdeb903a4a8a78032ec14931d06063b8
Alexander Wingård <alexander.wingard@gmail.com>
2016-02-11 15:33:54 +0100

    Handle TCP a little more robustly

--- src/erliot_registry.erl
+++ src/erliot_registry.erl
@@ -52,7 +52,12 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 client(IP, Port) ->
-    {ok, Socket} = gen_tcp:connect(IP, Port, [{active, true}, {packet, 2}, binary]),
+    {ok, Socket} = gen_tcp:connect(IP, Port, [{active, true},
+                                              {packet, 2},
+                                              {exit_on_close, true},
+                                              {send_timeout, 5000},
+                                              {send_timeout_close, true},
+                                              binary]),
     gproc:reg({n, l, {tcp_serial, IP, Port}}),
     gproc:reg({p, l, ?erliot_bcast}),
     client_loop(Socket).
@@ -62,18 +67,26 @@ client_loop(Socket) ->
         #?erliot_bcast{from = F} when F == self() ->
             client_loop(Socket);
         #?erliot_bcast{data = Msg} ->
-            client_handle(Socket, Msg)
+            client_handle(Socket, Msg);
+        Other ->
+            client_handle(Socket, Other)
     end.
 
-client_handle(Socket, JSON) ->
-    Term = jiffy:decode(JSON, [return_maps]),
-    Bin = term_to_binary(Term),
-    gen_tcp:send(Socket, Bin),
-    client_loop(Socket);
 client_handle(Socket, {tcp, Sock, Data}) ->
     Term = binary_to_term(Data),
-    JSON = jiffy:encode(Term, [return_maps]),
+    JSON = jiffy:encode(Term),
     gproc:bcast({p, l, ?erliot_bcast}, #?erliot_bcast{from = self(), data = JSON}),
     client_loop(Socket);
 client_handle(Socket, {tcp_closed, _}) ->
-    gen_tcp:close(Socket).
+    gen_tcp:close(Socket);
+client_handle(Socket, {tcp_error, _, Reason}) ->
+    gen_tcp:close(Socket);
+client_handle(Socket, JSON) ->
+    Term = jiffy:decode(JSON, [return_maps]),
+    Bin = term_to_binary(Term),
+    case gen_tcp:send(Socket, Bin) of
+        ok ->
+            client_loop(Socket)
+    end.
+
+
